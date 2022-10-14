32eae24f746e516bdd7fea591ebbdc5a964e9c5a
Maksim Hrudyn <snowjotunn@gmail.com>
2022-10-14 21:34:11 +0300

    Add day 7_1 problem solution

---
+++ 2015/07/1.py
@@ -0,0 +1,58 @@
+import re
+
+instructions = {}
+
+def get_instruction_value(id):
+	value = instructions[id]
+	if type(value) is not int:
+		value = value.strip()
+	else:
+		return value
+	command = re.findall(r'AND|OR|NOT|RSHIFT|LSHIFT', value)
+	if len(command) == 0:
+		numbers = re.findall(r'\d+', value)
+		if len(numbers) > 0:
+			instructions[id] = int(numbers[0])
+			return int(numbers[0])
+		else:
+			return get_instruction_value(value)
+	else:
+		command = command[0]
+		parts = value.split(command)
+		part_a = parts[0].strip()
+		part_b = parts[1].strip()
+		signal_a = 0
+		signal_b = 0
+		if len(part_a) > 0:
+			numbers_a = re.findall(r'\d+', part_a)
+			signal_a = int(numbers_a[0]) if len(numbers_a) > 0 else get_instruction_value(part_a)
+		if len(part_b) > 0:
+			numbers_b = re.findall(r'\d+', part_b)
+			signal_b = int(numbers_b[0]) if len(numbers_b) > 0 else get_instruction_value(part_b)
+
+		signal = 0
+		match command:
+			case 'AND':
+				signal = signal_a & signal_b
+			case 'OR':
+				signal = signal_a | signal_b
+			case 'NOT':
+				signal = ~ signal_b
+			case 'RSHIFT':
+				signal = signal_a >> signal_b
+			case 'LSHIFT':
+				signal = signal_a << signal_b
+			case _:
+				pass
+
+		instructions[id] = signal
+		return signal
+
+
+with open('input.txt', 'r') as f:
+	for line in f:
+		splited_line = line.rstrip().split("->")
+		instructions[splited_line[1].lstrip()] = splited_line[0]
+
+
+print(get_instruction_value('a'))
